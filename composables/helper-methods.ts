d314386ead402bc05c7807cf745897ef90db731b
Federico <fed_v@hotmail.com>
2024-07-13 14:33:15 -0400

    Convert composable to TypeScript

--- composables/helper-methods.ts
+++ composables/helper-methods.ts
@@ -1,5 +1,5 @@
 // Takes a date and displays it in yyyy-mm-dd format
-export const displayDate = (dateString) => {
+export const displayDate = (dateString: string): string => {
 
     const date = new Date(dateString);
     const options = { month: 'short', day: '2-digit', year: 'numeric', timeZone: 'UTC' };
